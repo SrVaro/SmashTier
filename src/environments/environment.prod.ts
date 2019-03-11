8f649d6d921909e8147ed381db09af6ed80cf7f7
Varo <sr.varo07@gmail.com>
2019-03-11 13:40:07 +0100

    Deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+
+  googleWebClientId: "893777304973-delpmuvktmrbh7uehecme940puig3atf.apps.googleusercontent.com",
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDTq180YZDEW9eFhDEQMiMJ6mbXa-S0XqI",
+    authDomain: "smashtier-821f4.firebaseapp.com",
+    databaseURL: "https://smashtier-821f4.firebaseio.com",
+    projectId: "smashtier-821f4",
+    storageBucket: "smashtier-821f4.appspot.com",
+    messagingSenderId: "893777304973",
+    personajesCollection: "personajes"
+
+  }
 };
